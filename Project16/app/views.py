3153f0451feb7004f5aef075d4d0d920c636a65d
Artsun T Kurian <artsunt2003@gmail.com>
2026-02-23 11:49:22 +0530

    Registation and save to DB with multiple ModelForms

--- Project16/app/views.py
+++ Project16/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from app.forms import *
 # Create your views here.
-
+from django.http import HttpResponse
 
 def insert(request):
     
@@ -13,6 +13,35 @@ def insert(request):
         'EWMFO':EWMFO,
         'EARMFO':EARMFO,
     }
+
+    if request.method=="POST":
+        TMFDO=TopicMF(request.POST)
+        WMFDO=WebpageMF(request.POST)
+        ARMFDO=AcessRecordMF(request.POST)
+
+        if TMFDO.is_valid() and WMFDO.is_valid() and ARMFDO.is_valid():
+            TMFDO=TMFDO.save()
+
+            print(TMFDO)
+
+
+            MWMFDO=WMFDO.save(commit=False)
+            MWMFDO.topic_name=TMFDO
+            MWMFDO.save()
+            
+            MARMFDO=ARMFDO.save(commit=False)
+            MARMFDO.name=MWMFDO
+            MARMFDO.save()
+
+            return HttpResponse('Data entered susessfully')
+        
+        else:
+            print(TMFDO)
+            print(WMFDO)
+            print(ARMFDO)
+            return HttpResponse("invalid")
+        
+        
 
     
     return render(request,'insert.html',d)
