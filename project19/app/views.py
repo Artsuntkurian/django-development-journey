ec57f6540299fedf560893533328c82564cf063b
Artsun T Kurian <artsunt2003@gmail.com>
2026-03-07 00:29:35 +0530

    Create view,update view ,delete view

--- project19/app/views.py
+++ project19/app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.models import *
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
-
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 
 class School_List(ListView):
@@ -19,3 +19,18 @@ class School_Detail(DetailView):
 def wish(request,name):
     print(name)
     return HttpResponse(f' Heloo {name}')
+
+
+class School_Create(CreateView):
+    model=School
+    fields="__all__"
+
+class School_Update(UpdateView):
+    model=School
+    fields="__all__"
+    
+
+class School_Delete(DeleteView):
+    model=School
+    context_object_name='schoolobj'
+    success_url=reverse_lazy('School_List')
--- project19/project19/urls.py
+++ project19/project19/urls.py
@@ -23,9 +23,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('School_List/',School_List.as_view(),name='School_List'),
 
+    path('School_Create/',School_Create.as_view(),name='School_Create'),
 
-    re_path('wish/(?P<name>.*)/',wish,name='wish'),
+#    re_path('wish/(?P<name>.*)/',wish,name='wish'),
 
-
+    re_path('update/(?P<pk>\d+)',School_Update.as_view(),name='School_Update'),
+    re_path('delete/(?P<pk>\d+)',School_Delete.as_view(),name='School_Delete'),
     re_path('(?P<pk>\d+)',School_Detail.as_view(),name='School_Detail'),
+
+    
 ]
